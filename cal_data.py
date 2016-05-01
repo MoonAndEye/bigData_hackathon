a0fbbdac9a7e1456bf309f009a12545bcc0e3e98
Moonandeye <atimis19@gmail.com>
2016-05-01 11:44:15 +0800

    prototype
    dadf

--- cal_data.py
+++ cal_data.py
@@ -5,7 +5,13 @@ Created on Sun May  1 09:22:59 2016
 @author: Moon
 """
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import os, sys
+import csv
+
+plt.rcParams['figure.figsize'] = (15, 3)
+plt.rcParams['font.family'] = 'sans-serif'
 
 merge_base = ['available' , 'id', 'time']#這個放要合併基準
 
@@ -31,5 +37,26 @@ def loadInPd(_path):
 
 result_df = loadInPd(history_list[0])
 result_df = result_df.set_index('id')
-result_df = result_df.drop(['available', 'time'], axis = 1)
-    
+result_df = result_df.drop(['available','time'], axis = 1)
+
+csv_path = "C:/1save/parking_test2.csv"
+
+plat_df = pd.read_csv(csv_path)
+#test = pd.DataFrame(loadInPd(history_list[0]))
+
+def addData(out, add_list):
+    temp = pd.DataFrame(loadInPd(add_list))
+    time_index = add_list[-21:-4]
+    temp = temp.set_index('id')
+    out[time_index] = temp['available']
+    return(out)
+"""
+test = addData(result_df,history_list[0])
+test2 = pd.DataFrame(loadInPd(history_list[0]))
+
+"""
+for all_csv in history_list:
+    result_df = addData(result_df, all_csv)    
+
+
+
